554211487dfbedcd9a28ca20381222532db110a9
jlescan0 <jlescanog1@upao.edu.pe>
2024-10-30 22:33:45 -0500

    1 commit foro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,8 @@
 import { Routes } from '@angular/router';
 import { ForumComponent } from './pages/forum/forum.component';
-import { RegisterComponent } from './pages/register/register.component';
-import { PaymentComponent } from './pages/payment/payment.component';
-
 export const routes: Routes = [
     {
         path: 'forum',
         component: ForumComponent
-    },
-    {
-        path: 'register',
-        component: RegisterComponent
-    },
-    {
-        path: 'payment',
-        component: PaymentComponent
     }
 ]
