62894d7a62316335fa819c2413ff96c6b65a3bf6
sthefano jimenez atoche <sjimeneza1@upao.edu.pe>
2024-11-04 09:40:57 -0500

    estructura de reporte

---
+++ src/app/pages/auth/auth-layuot/auth-layuot.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-layuot',
+  standalone: true,
+  imports: [],
+  templateUrl: './auth-layuot.component.html',
+  styleUrl: './auth-layuot.component.scss'
+})
+export class AuthLayuotComponent {
+
+}
