9f6c97fdb650156c8591bb19c63168f03b67d627
Alexander Prieto <aprietom1@upao.edu.pe>
2024-11-09 00:23:25 -0500

    Creación de formulario Registrar recurso

---
+++ src/app/Pages/register-resource/register-resource.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {MatError, MatFormField, MatLabel} from '@angular/material/form-field';
+import {MatInput} from '@angular/material/input';
+import {MatButton} from '@angular/material/button';
+import {MatOption} from '@angular/material/core';
+import {NgForOf, NgIf} from '@angular/common';
+import {MatSelect} from '@angular/material/select';
+import {MatCard, MatCardContent, MatCardHeader, MatCardSubtitle, MatCardTitle} from '@angular/material/card';
+import { LayoutDashboardComponent} from '../../shared/components/layout-dashboard/layout-dashboard.component';
+
+@Component({
+  selector: 'app-register-resource',
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    MatFormField,
+    MatInput,
+    MatButton,
+    MatOption,
+    NgIf,
+    MatSelect,
+    NgForOf,
+    MatLabel,
+    MatError,
+    MatCardContent,
+    MatCardSubtitle,
+    MatCardHeader,
+    MatCard,
+    MatCardTitle,
+    LayoutDashboardComponent
+  ],
+  templateUrl: './register-resource.component.html',
+  styleUrl: './register-resource.component.scss'
+})
+export class RegisterResourceComponent {
+  resourceForm: FormGroup;
+  tiposDeRecurso: string[] = ['Digital', 'Físico'];
+
+  constructor(private fb: FormBuilder) {
+    this.resourceForm = this.fb.group({
+      nombreRecurso: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(100)]],
+      descripcionRecurso: ['', [Validators.minLength(10), Validators.maxLength(500)]],
+      precioRecurso: [0, [Validators.required, Validators.min(0)]],
+      tipoRecurso: ['', Validators.required],
+      tipoDeHabitId: ['', Validators.required]
+    });
+  }
+
+  OnInit(): void {}
+
+  onSubmit(): void {
+    if (this.resourceForm.valid) {
+      console.log("Recurso registrado:", this.resourceForm.value);
+      // Aquí puedes añadir la lógica para enviar el formulario a un servicio
+    } else {
+      console.error("Formulario inválido");
+    }
+  }
+
+  onCancel() {
+    // Lógica para cancelar, por ejemplo, redirigir a otra página o limpiar el formulario
+  }
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { PhysicalComponent } from './Pages/physical/physical.component';
 import { passwordRecoveryRoutes} from './Pages/password-recovery-request/password-recovery-request.routes';
 import { passwordRecoveryConfirmationRoutes } from './Pages/password-recovery-confirmation/password-recovery-confirmation.routes';
 import { passwordResetRoutes} from './Pages/password-reset/password-reset.routes';
+import { registerResourceRoutes} from './Pages/register-resource/register-resource.routes';
 
 export const routes: Routes = [
   { path: 'reg', component: PhysicalComponent},
   ...passwordRecoveryConfirmationRoutes,
   ...passwordRecoveryRoutes,
   ...passwordResetRoutes,
+  ...registerResourceRoutes,
   { path: '**', redirectTo: '/reg' },
 ];
