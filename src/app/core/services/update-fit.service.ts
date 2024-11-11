b48dfa4365350c9e01d8ab76ef8b370641288a3c
jlescan0 <jlescanog1@upao.edu.pe>
2024-11-11 09:23:21 -0500

    uf2

---
+++ src/app/core/services/update-fit.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UpdateFitService {
+  private storageKey = 'fitnessMetrics';
+
+  getMetrics(): { weight: number; height: number; date: string }[] {
+    const savedMetrics = localStorage.getItem(this.storageKey);
+    return savedMetrics ? JSON.parse(savedMetrics) : [];
+  }
+
+  saveMetrics(metrics: { weight: number; height: number; date: string }[]): void {
+    localStorage.setItem(this.storageKey, JSON.stringify(metrics));
+  }
+}
--- src/app/pages/update-fit/update-fit.component.ts
+++ src/app/pages/update-fit/update-fit.component.ts
@@ -1,14 +1,43 @@
-import { Component } from '@angular/core';
-import { FooterComponent } from '../../shared/components/footer/footer.component';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { NavbarComponent } from '../../shared/components/navbar/navbar.component';
+import { FooterComponent } from '../../shared/components/footer/footer.component';
+import { UpdateFitService } from '../../core/services/update-fit.service';
 
 @Component({
   selector: 'app-update-fit',
   standalone: true,
-  imports: [FooterComponent, NavbarComponent],
+  imports: [CommonModule, FormsModule, NavbarComponent, FooterComponent],
   templateUrl: './update-fit.component.html',
-  styleUrl: './update-fit.component.scss'
+  styleUrls: ['./update-fit.component.scss']
 })
-export class UpdateFitComponent {
+export class UpdateFitComponent implements OnInit {
+  weight: number | null = null;
+  height: number | null = null;
+  metricEntries: { weight: number; height: number; date: string }[] = [];
 
+  constructor(private updateFitService: UpdateFitService) {}
+
+  ngOnInit() {
+    this.loadMetrics();
+  }
+
+  updateMetrics() {
+    if (this.weight && this.height) {
+      const newEntry = {
+        weight: this.weight,
+        height: this.height,
+        date: new Date().toLocaleDateString()
+      };
+      this.metricEntries.push(newEntry);
+      this.updateFitService.saveMetrics(this.metricEntries);
+      this.weight = null;
+      this.height = null;
+    }
+  }
+
+  loadMetrics() {
+    this.metricEntries = this.updateFitService.getMetrics();
+  }
 }
