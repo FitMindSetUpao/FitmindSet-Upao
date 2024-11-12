86faa2a36d4f9b2b5259c60d7ad15b63390b15c8
sthefano jimenez atoche <sjimeneza1@upao.edu.pe>
2024-11-11 23:48:34 -0500

    Components del shared

---
+++ src/app/shared/components/Layout/Layout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-layout',
+  templateUrl: './layout.component.html',
+  standalone: true,
+  styleUrls: ['./layout.component.scss']
+})
+export class LayoutComponent {
+
+
+}
