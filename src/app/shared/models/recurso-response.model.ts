90d9d9df6cd6f667bf078c309bf7cb44854871b8
Sebastian Alonso Saavedra Arroyo <165932627+SaavedraSebastian@users.noreply.github.com>
2024-11-29 02:56:30 -0500

    Update recurso-response.model.ts

--- src/app/shared/models/recurso-response.model.ts
+++ src/app/shared/models/recurso-response.model.ts
@@ -1,14 +1,13 @@
 export interface RecursoResponse{
-    recursoid:number;
-    nombreRecurso:string;
-    descripcionRecurso:string;
-    precioRecurso:number;
-    tipoRecurso:string;
-    autorNombre:string;
-    coverPath: string;
-    filePath: string;
-    createdAt: string;
-    updatedAt: string;
-    tipoDeHabito:string;
-
+  recursoId:number;
+  nombreRecurso:string;
+  descripcionRecurso:string;
+  precioRecurso:number;
+  tipoRecurso:string;
+  autorNombre:string;
+  coverPath: string;
+  filePath: string;
+  createdAt: string;
+  updatedAt: string;
+  tipoDeHabito:string;
 }
