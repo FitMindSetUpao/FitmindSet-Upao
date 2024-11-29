31d77cc7289a56e10fa42d6a024973eb8a12b208
Sebastian Alonso Saavedra Arroyo <165932627+SaavedraSebastian@users.noreply.github.com>
2024-11-29 01:35:19 -0500

    Update seguimiento.service.ts

--- src/app/core/services/seguimiento.service.ts
+++ src/app/core/services/seguimiento.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
-import { SeguimientoDTO } from '../../shared/models/seguimiento.model';
+import { SeguimientoDTO } from '../../../../../shared/models/seguimiento.model';
 import { ReporteDTO } from '../../shared/models/reporte.model';
 import { SeguimientoResponse } from '../../shared/models/seguimiento-response.model';
 
