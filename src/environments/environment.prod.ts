8f3ae76b62c1808b507cd14bccbca052e13c1e17
Sebastian Alonso Saavedra Arroyo <165932627+SaavedraSebastian@users.noreply.github.com>
2024-11-29 11:56:04 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment={
     production: true,
-    baseURL: 'https://fitmindset-latest.onrender.com/api/v1'
+    baseURL: 'https://fitmindset-latest.onrender.com/api/v1',
+ paypalReturnUrl: 'http://localhost:4200/customer/pagos/cart'
 };
