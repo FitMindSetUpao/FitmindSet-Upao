54bb539b635a60fa9c8ec47f4b126ee3c7176fe7
Sebastian Alonso Saavedra Arroyo <165932627+SaavedraSebastian@users.noreply.github.com>
2024-11-29 01:50:06 -0500

    Update habito.model.ts

--- src/app/shared/models/habito.model.ts
+++ src/app/shared/models/habito.model.ts
@@ -1,7 +1,11 @@
+import { MetaDTO } from "./meta.model";
 export interface Habito {
     id?: number;
     nombreHabito: string;
     descripcion: string;
     tipoDeHabitoId: number;
+    metas: MetaDTO[]; 
+    fechaCreacion: string; 
+    
   }
   
