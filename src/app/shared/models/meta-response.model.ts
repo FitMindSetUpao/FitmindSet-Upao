d7e85bba989acd56bbbb05e2d727ed9b3ad10371
Sebastian Alonso Saavedra Arroyo <165932627+SaavedraSebastian@users.noreply.github.com>
2024-11-29 02:31:33 -0500

    Update meta-response.model.ts

--- src/app/shared/models/meta-response.model.ts
+++ src/app/shared/models/meta-response.model.ts
@@ -1,12 +1,14 @@
-import { MetaDTO } from "./meta.model";
-export interface MetaResponseDTO {
-    id: number;
-    descripcion: string;
-    estado: string;
-    fechaInicio: Date;  
-    fechaFin: Date;   
-    habitoId: number;
-    tiempoObjetivo: number;
-    metas: MetaDTO[];
+import { SeguimientoDTO } from "./seguimiento.model";  // Ajusta la ruta según sea necesario
+
+export interface MetaDTO {
+  id?:number;
+  descripcion: string;
+  estado: string;
+  fechaInicio: string; // Formato ISO string
+  fechaFin: string;    // Formato ISO string
+  tiempoObjetivo: number;
+  habitoId: number;
+  customerId?: number; 
+  seguimientos?: SeguimientoDTO[]; 
   }
   
