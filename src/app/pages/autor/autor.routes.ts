041458d7e49c49a997d18130bb87393862d94db9
DioghoBaylon <165933030+DioghoBaylon@users.noreply.github.com>
2024-11-28 22:17:56 -0500

    Update autor.routes.ts

--- src/app/pages/autor/autor.routes.ts
+++ src/app/pages/autor/autor.routes.ts
@@ -1,19 +1,20 @@
-import { Routes } from "@angular/router";
-import { LayoutComponent } from "./layout/layout.component";
-import { RecursoListComponent } from "./recurso-management/recurso-list/recurso-list.component";
-import { RecursoFormComponent } from "./recurso-management/recurso-form/recurso-form.component";
-import { UserProfileComponent } from "../../shared/components/user-profile/user-profile.component";
-import { AuthLayoutComponent } from "../auth/auth-layout/auth-layout.component";
+import { Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
+import { RecursoListComponent } from './recurso-management/recurso-list/recurso-list.component';
+import { RecursoFormComponent } from './recurso-management/recurso-form/recurso-form.component';
+import { UserProfileComponent } from '../../shared/components/user-profile/user-profile.component';
+import { AuthLayoutComponent } from '../auth/auth-layout/auth-layout.component';
+
 export const autorRoutes: Routes = [
-    {
-        path: '',
-        component: LayoutComponent,
-        children: [
-            {path: 'recursos/crear',component: RecursoFormComponent},
-            {path: 'recursos/edit/:recursoid',component: RecursoFormComponent},
-            {path: 'recursos/list', component: RecursoListComponent},
-            {path: 'mi-perfil', component: UserProfileComponent},
-            {path: 'mi-dashboard', component: AuthLayoutComponent},
-        ]
-    }
-]
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: 'recursos/crear', component: RecursoFormComponent },
+      { path: 'recursos/edit/:id', component: RecursoFormComponent },
+      { path: 'recursos/list', component: RecursoListComponent },
+      { path: 'mi-perfil', component: UserProfileComponent },
+      { path: 'mi-dashboard', component: AuthLayoutComponent },
+    ],
+  },
+];
